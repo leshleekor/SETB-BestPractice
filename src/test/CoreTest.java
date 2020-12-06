f9b06b96e633c24db29d8c875c751ab5fbb9dbbf
Lesh Lee <lesh.lee.kor@gmail.com>
2020-12-06 18:51:06 +0900

    BEST-27 test/Core test Commit

---
+++ src/test/CoreTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import engine.Core;
+
+public class CoreTest {
+    @Test
+    void testTest() {
+
+    }
+}
