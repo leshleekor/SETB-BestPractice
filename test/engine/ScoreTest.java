37aa3ed4bb9a3d4950dde2b57df34456de4f18e5
lolmc <illionaire1004@gmail.com>
2020-12-08 19:27:00 +0900

    difficulty 설정관련 클래스 Test 코드 작성

---
+++ test/engine/ScoreTest.java
@@ -0,0 +1,44 @@
+package engine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class ScoreTest {
+
+    /** Player's name. */
+    private String name;
+    /** Score points. */
+    private int playerScore;
+    private Score score;
+
+    @BeforeEach
+    void setUp(){
+        name = "ABC";
+        playerScore = 10000;
+        score = new Score(name, playerScore);
+    }
+
+    @Test
+    void TestGetName_ShouldReturnCurrentName() {
+        String expected = this.name;
+        String actual = score.getName();
+        assertEquals(expected, actual, "getName should return current player name");
+    }
+
+    @Test
+    void TestGetScore_ShouldReturnCurrentScore() {
+        int expected = this.playerScore;
+        int actual = score.getScore();
+        assertEquals(expected, actual, "getCurrentScore should return current score");
+    }
+
+    @Test
+    void TestCompareTo_ShouldReturnValidCompareInteger(){
+        assertTrue(score.compareTo(score) == 1 ||
+                score.compareTo(score) == 0 ||
+                score.compareTo(score) == -1, "compareTo should return -1 or 0 or 1");
+    }
+}
